817d459d8f67e4f1c16b4af018743e620a91a85f
Leof07 <fb17006@ues.edu.sv>
2024-02-12 21:03:05 -0600

    first commit

---
+++ app/Models/Alumno.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Alumno extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'fechaNacimiento',
+        'direccion',
+        'telefono',
+        'curso_id',
+    ];
+    public function curso(): HasOne
+    {
+        return $this->hasOne(Curso::class,'id','curso_id');
+    }
+}
